221bb55195fa56d8a7ab281833662c2e7736ee24
kellytbn <kellytbn14@gmail.com>
2023-05-27 16:27:24 -0500

    Se implementa tabla normal

--- pages/app.tsx
+++ pages/app.tsx
@@ -2,7 +2,7 @@ import Layout from "@/layouts/Layout";
 import Head from "next/head";
 import React from "react";
 import { InventoryContextProvider } from "@/context/inventoryContext";
-import InventoryManagement from "@/pages/InventoryManagement";
+import InventoryManagement from "@/pages/inventory-management";
 
 const app = () => (
   <>
