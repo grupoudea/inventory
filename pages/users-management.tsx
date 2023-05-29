efb232277518f08b2d5e76a93933a385fc4b9eef
berracode <juanguillermoalarcon@gmail.com>
2023-05-29 00:30:47 -0500

    se agrega login y rutas privadas

--- pages/users-management.tsx
+++ pages/users-management.tsx
@@ -9,17 +9,16 @@ import { Column } from "@/utils/utils";
 import TableUsers from "@/components/TableUsers";
 import { useQuery } from "@apollo/client";
 import { GET_USERS } from "@/graphql/client/user";
-import { Profile, User } from "@prisma/client";
-import TableReactDataGrid from "@/components/TableReactDataGrid";
+import PrivateRoute from "@/components/PrivateRoute";
 
 const UsersManagementPage = () => (
-  <>
+  <PrivateRoute>
     <Layout>
       <InventoryContextProvider>
         <UsersManagement />
       </InventoryContextProvider>
     </Layout>
-  </>
+  </PrivateRoute>
 );
 
 const UsersManagement = () => {
@@ -43,15 +42,13 @@ const UsersTable = () => {
   // TODO Tambien el calculo de la cantidad disponible.
   // TODO organizar el objeto response de la forma de [datos]
 
-  const {data,loading,error} = useQuery<{users:any[]}>(
-    GET_USERS, {
-      fetchPolicy: 'cache-first',
-    }
-  );
+  const { data, loading, error } = useQuery<{ users: any[] }>(GET_USERS, {
+    fetchPolicy: "cache-first",
+  });
 
-  let users : any[] = data?.users || [];
+  let users: any[] = data?.users || [];
   // console.log(users);
-  
+
   const columns: Column[] = [];
   columns.push({ name: "id", header: "Identificador" });
   columns.push({ name: "creation_date", header: "Fecha de creación" });
@@ -61,8 +58,7 @@ const UsersTable = () => {
   return (
     <>
       <TableUsers dataSource={users} columns={columns} />
-      <div className="flex justify-end pr-0 mt-5 text-lg mb-24">
-      </div>
+      <div className="flex justify-end pr-0 mt-5 text-lg mb-24"></div>
     </>
   );
 };
