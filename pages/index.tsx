41de41284ff66c6ed405e05181bf98a4bbb9a76d
berracode <juanguillermoalarcon@gmail.com>
2023-05-29 00:41:49 -0500

    se agrega router

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import { signIn, useSession } from "next-auth/react";
 import React from "react";
 import { MdDataset } from "react-icons/md";
+import { useRouter } from "next/router";
 
 const index = () => {
+  const router = useRouter();
   const { data: session } = useSession();
   console.log("session", session);
 
