cdcf6a1208a909d1fdb885986dc45b21fe73fd31
berracode <juanguillermoalarcon@gmail.com>
2023-05-31 19:11:19 -0500

    se agrega pantalla login oscura por defecto

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const index = () => {
   }
 
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-gray-50 dark:bg-gray-800 text-white ">
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-gray-800 text-white ">
       <div className="w-96 flex flex-col flex-auto justify-center items-center">
         <div className="mb-5">
           <MdDataset size={70} />
